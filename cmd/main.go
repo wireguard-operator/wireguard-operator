60a3ba20d3449ba90e093f56024360c644a80426
Julian Schlarb <julian.schlarb@denktmit.de>
2025-10-07 10:59:05 +0200

    feat: add WireGuardPeer API and controller
    Command:
    operator-sdk create api --version v1alpha1 --kind WireGuardPeer --resource --controller

--- cmd/main.go
+++ cmd/main.go
@@ -217,6 +217,13 @@ func main() {
 			os.Exit(1)
 		}
 	}
+	if err := (&controller.WireGuardPeerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "WireGuardPeer")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
